bbf9928f6ff804ad096f3f86f7510dd5d61998a2
Kareem Ellis <kareemellis55@gmail.com>
2023-04-14 23:51:34 -0500

    Imported dependencies to __init__

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from .config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+csrf = CSRFProtect(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from app import views
