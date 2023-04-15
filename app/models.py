90e27f624d0f0200a5d01c9c35d261777483ba40
Kareem Ellis <kareemellis55@gmail.com>
2023-04-15 00:54:49 -0500

    Created Post model and imported to views.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,26 @@
 # Add any model classes for Flask-SQLAlchemy here
+from . import db
+import datetime
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    caption = db.Column(db.String(250))
+    photo = db.Column(db.String(250))
+    user_id = db.Column(db.Integer)
+    created_on = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def __init__(self, caption, photo, user_id):
+        self.caption = caption
+        self.photo = photo
+        self.user_id = user_id
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<post: %r>' % (self.id)
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ This file creates your application.
 
 from app import app
 from flask import render_template, request, jsonify, send_file
+from app.models import Post
 import os
 
 
