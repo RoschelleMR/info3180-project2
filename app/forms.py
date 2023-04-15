1fd74352c89ec5a7acb45072e972b86611a5fb27
Kareem Ellis <kareemellis55@gmail.com>
2023-04-15 01:13:03 -0500

    Added PostForm to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,13 @@
 # Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField, TextAreaField
+from wtforms.validators import InputRequired
+from flask_wtf.csrf import CSRFProtect
+
+class PostForm(FlaskForm):
+    caption = StringField('Caption', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[
+            FileRequired(),
+            FileAllowed(['jpg', 'png'], 'Images only!')
+        ])
