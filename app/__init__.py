580f295e06a5da11bc8e43bfd8d4320eaed0327e
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-16 17:46:15 -0500

    Added flask login to init

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,5 +11,10 @@ app.config.from_object(Config)
 csrf = CSRFProtect(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 from app import views
