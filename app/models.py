09d04c73dea203daa9103f9fd900b5357848dc3f
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-16 16:54:08 -0500

    Implemented Users model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,55 @@
 # Add any model classes for Flask-SQLAlchemy here
 from . import db
 import datetime
+from werkzeug.security import generate_password_hash
+
+class Users(db.Model):
+    
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    first_name = db.Column(db.String(100))
+    last_name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    location = db.Column(db.String(250))
+    biography = db.Column(db.String(250))
+    profile_photo = db.Column(db.String(100))
+    joined_on = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    
+    
+    def __init__(self, username, password, first_name, last_name, 
+                 email, location, biography, profile_photo):
+        
+        self.username = username
+        self.password = generate_password_hash(password, method="sha256")
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.location = location
+        self.biography = biography
+        self.profile_photo = profile_photo
+        
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id)  # python 2 support
+        except NameError:
+            return str(self.id)  # python 3 support
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+    
 
 class Post(db.Model):
     __tablename__ = 'posts'
