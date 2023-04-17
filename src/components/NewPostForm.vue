23fc879598256150795fcb417a20eae1ba73199a
Kimberly Laughton <kimberlyclaughton@gmail.com>
2023-04-17 15:39:04 -0500

    Added vue files for login and post forms

---
+++ src/components/NewPostForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <form @submit.prevent = "submitForm" id = "NewPostForm">
+        <h1>New Post</h1>
+        <div class = "photoCaption"
+            <div class = "photo">
+                <label for="photo" class="form-label">Photo</label>
+                <input type = "file" id = "img" name = "img" class = "formcontrol" accept = ".jpg, .png"/>
+            </div>
+            <div class = "caption"
+                <label for="caption" class="form-label">Caption</label>
+                <textarea name="caption" class="formcontrol" ></textarea>
+            </div>
+            <button>Submit</button>
+        </div>
+    </form>
+</template>
+            
