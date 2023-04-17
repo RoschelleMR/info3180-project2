b9d0d7678fc19a4002a85b8b9549e9efa3eb6425
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-17 12:48:35 -0500

    Implemented token security

--- app/views.py
+++ app/views.py
@@ -6,14 +6,84 @@ This file creates your application.
 """
 
 from app import app, db, login_manager
-from flask import render_template, request, jsonify, send_file, send_from_directory
+from flask import render_template, request, jsonify, send_file, send_from_directory, g
 from werkzeug.utils import secure_filename
 from flask_wtf.csrf import generate_csrf
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.security import check_password_hash
 from app.forms import PostForm, LoginForm
 from app.models import Post, Users
-import os
+import os, jwt
+from functools import wraps
+from datetime import datetime, timedelta
+
+
+
+###
+# SECURITY FUNCTIONS
+###
+
+
+# generates csrf token
+@app.route('/api/v1/csrf-token', methods=['GET'])
+def get_csrf():
+    return jsonify({'csrf_token': generate_csrf()})
+
+
+
+# decorator to allow route functions to check if a valid jwt token was found
+def requires_auth(f):
+    
+  @wraps(f)
+  
+  def decorated(*args, **kwargs):
+    
+    # Note: might use cookie instead since it's easier to set an expiration time
+    
+    auth = request.headers.get('Authorization', None) # or request.cookies.get('token', None)
+
+    if not auth:
+      return jsonify({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'}), 401
+
+    parts = auth.split()
+
+    if parts[0].lower() != 'bearer':
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}), 401
+    elif len(parts) == 1:
+      return jsonify({'code': 'invalid_header', 'description': 'Token not found'}), 401
+    elif len(parts) > 2:
+      return jsonify({'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}), 401
+
+    token = parts[1]
+    try:
+        payload = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({'code': 'token_expired', 'description': 'token is expired'}), 401
+    except jwt.DecodeError:
+        return jsonify({'code': 'token_invalid_signature', 'description': 'Token signature is invalid'}), 401
+
+    g.current_user = user = payload
+    return f(*args, **kwargs)
+
+  return decorated
+
+
+@app.route("/api/v1/generate-token")
+def generate_token():
+    timestamp = datetime.utcnow()
+    payload = {
+        "sub": 1,
+        "iat": timestamp,
+        "exp": timestamp + timedelta(minutes=3)
+    }
+
+    token = jwt.encode(payload, app.config['SECRET_KEY'], algorithm='HS256')
+
+    return jsonify(token=token)
+
+
+
 
 
 ###
@@ -23,10 +93,6 @@ import os
 @app.route('/')
 def index():
     return jsonify(message="This is the beginning of our API")
-
-@app.route('/api/v1/csrf-token', methods=['GET'])
-def get_csrf():
-    return jsonify({'csrf_token': generate_csrf()})
 
 @app.route('/api/v1/users/<user_id>/posts', methods=['GET'])
 def userPosts(user_id):
@@ -77,7 +143,7 @@ def getPoster(filename):
 
 
 # Login route
-@app.route('/api/v1/login', methods=['POST'])
+@app.route('/api/v1/auth/login', methods=['POST'])
 def login():
     
     form = LoginForm()
@@ -122,8 +188,10 @@ def login():
    
 #Logout route
 
-@app.route("/api/v1/logout")
+@requires_auth
+@app.route("/api/v1/auth/logout", methods=['POST'])
 def logout():
+    
     
     logout_user()
     
