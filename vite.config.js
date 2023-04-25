4fd96993ec35acdba1614cfb72e41dc65c3bd1eb
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-24 23:41:51 -0500

    Added api proxy

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '^/api*': {
+        target: 'http://127.0.0.1:8080/'
+      }
+    }
   }
 })
