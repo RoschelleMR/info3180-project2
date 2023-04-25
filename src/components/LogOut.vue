cf11f53389175f635638278446af26719c48dd16
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-25 06:34:18 -0500

    Implemented front end for logout

---
+++ src/components/LogOut.vue
@@ -0,0 +1,68 @@
+<template>
+
+    <div v-if = "logoutStatus == 'success'" class="alert alert-success">
+      <p>Successfully Logged Out</p>
+    </div>
+
+    <div class="card">
+        <h3>
+            Are you sure you want to logout out?
+        </h3>
+        <button @click="logout" class="button">Logout</button>
+    </div>
+    
+</template>
+
+<script setup>
+
+
+  import { ref, onMounted } from "vue"; onMounted(() => {  
+        getCsrfToken();   
+    }); 
+
+    let logoutStatus = ref("");  
+    let csrf_token = ref("");
+
+     
+    // let token = localStorage.getItem('token')
+    let token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImlhdCI6MTY4MjQyMjI1OSwiZXhwIjoxNjgyNDIzMTU5fQ.6K7KNYlr9T4GkNpY2SYJg1j4F9YOVNtXSWrsxiyPdo4"
+    console.log(token)
+
+    function getCsrfToken() {     
+        fetch('/api/v1/csrf-token')       
+        .then((response) => response.json())       
+        .then((data) => {         
+            console.log(data);         
+            csrf_token.value = data.csrf_token;       
+        })   
+    } 
+
+    function logout() {     
+        
+        localStorage.setItem('isLogin', false );
+
+        fetch("/api/v1/auth/logout", {     
+            method: 'POST',
+            headers: {             
+                'X-CSRFToken': csrf_token.value,
+                'Authorization': "Bearer " + token     
+                }  
+            })    
+        .then((response) => 
+            response.json())  
+
+        .then((data) => {         
+            console.log(data); 
+            if (data.hasOwnProperty('success')){
+              logoutStatus.value = "success";  
+            }        
+                
+        })   
+    }
+
+     
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -21,6 +21,11 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: () => import('../views/LogoutView.vue')
     }
   ]
 })
