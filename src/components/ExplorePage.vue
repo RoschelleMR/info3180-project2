0bc20eb2a283e21f060971cf69393fd46d1c2b82
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-04-28 11:48:57 -0500

    Initial commit to ExplorePage branch

---
+++ src/components/ExplorePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="exploreSidebar">
+        <button>New Post</button>
+    </div>
+
+    <div class="exploreMainBar">
+        <!-- Other Users' Posts -->
+    </div>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ src/views/ExploreView.vue
@@ -0,0 +1,7 @@
+<script setup>
+    import ExplorePage from "@/components/ExplorePage.vue";
+</script>
+
+<template>
+    <ExplorePage />
+</template>
