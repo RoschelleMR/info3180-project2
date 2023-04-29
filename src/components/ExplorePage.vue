45f24c6c68ef56311b26741abdd088a4237799f9
Jamilla Morgan <jamillamorgan02@gmail.com>
2023-04-28 20:31:45 -0500

    Populated the Home page before styling

--- src/components/ExplorePage.vue
+++ src/components/ExplorePage.vue
@@ -14,6 +14,7 @@
 
 <script>
     import axios from 'axios';
+    import { ref } from "vue";
 
     export default {
     data() {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <script setup>
 import { ref } from "vue";
 
-let message = ref("Hello World! This is a VueJS and Flask Starter Template.")
+let message = ref("Photogram")
+let subtext = ref("Share photos of your favourite moments with friends, family and the world.")
 
 </script>
 
 <template>
     <div class="container">
       <div class="text-center">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/Quote.jpg" width="125" height="125" />
         <h1>{{ message }}</h1>
+        <p>{{ subtext }}</p>
+        <button>Register</button>
+        <br>
+        <button>Login</button>
       </div>
     </div>
 </template>
