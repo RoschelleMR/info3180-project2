1f6fc4207a4f02b33d9db03f64cc7607bd3be775
Roschelle Matthews-Rhoden <roschelle2k6@gmail.com>
2023-04-30 23:33:45 -0500

    Implemented front end for homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,88 @@
 <script setup>
 import { ref } from "vue";
+import {useRouter} from "vue-router";
+const router = useRouter()
 
-let message = ref("Hello World! This is a VueJS and Flask Starter Template.")
+
 
 </script>
 
 <template>
     <div class="container">
-      <div class="text-center">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-        <h1>{{ message }}</h1>
+
+      <div class="card">
+        <img src="@/assets/home-img.jpg" alt=""/>
       </div>
+
+      <div class="card info">
+        <div class="title">
+          <h3 >Photogram</h3>
+        </div>
+        <hr>
+        <div class="card-body">
+          <p>Share photos of your favourite moments with friends, family and the world</p>
+          <div class="button-box">
+            <button @click="$router.push('/register')" class="btn register">Register</button>
+            <button @click="$router.push('/login')" class="btn btn-primary login">Login</button>
+          </div>
+        </div>
+      </div>
+
     </div>
 </template>
 
 <style>
-/* Add any component specific styles here */
+  .container{
+    display: flex;
+    column-gap: 8px;
+    align-content: center;
+    justify-content: center;
+  }
+
+  .title{
+    text-align: center;
+  }
+
+  h3{
+    font-weight: bold;
+  }
+
+  .info{
+    padding: 25px;
+  }
+
+  .button-box{
+    display: flex;
+    justify-content: center;
+    column-gap: 5px;
+  }
+
+  .register{
+    background-color: rgb(7, 202, 43);
+        color: white;
+        font-weight: bold;
+
+        border: none;
+        border-radius: 10px;
+
+        width: 100%;
+        margin-top: 10px;
+        padding: 6px;
+  }
+
+  .register:hover{
+    background-color: rgb(4, 220, 44);
+    color: white;
+    transition: all 0.8s;
+  }
+
+  .login{
+
+    font-weight: bold;
+
+    width: 100%;
+    margin-top: 10px;
+    padding: 6px;
+
+  }
 </style>
